a3bfe6475a68d1eb01e558910c3526a18336c4b3
Shane Wiens <Shanewiens@gmail.com>
2022-04-16 16:44:11 -0700

    made the writeReadMe function

--- index.js
+++ index.js
@@ -74,6 +74,38 @@ const questions = [
 
 
 //Function to write README file
+function writeReadMe() {
+    inquirer
+        .prompt(questions)
+        .then((responses) => {
+            console.log(responses);
+            const myMarkdown = markdown(responses.license);
+            fs.writeFile('newREADME.md', `# ${responses.title}
+## Description
+${responses.description}
+## Table of Contents
+1. [Installation](#installation) 
+2. [Usage](#usage)
+3. [Contributions](#contributions)
+4. [Questions](#questions)
+## Installation 
+${responses.installation}
+## Usage 
+${responses.usage}
+## Contributing 
+${responses.contributing}
+## Questions
+### GitHub
+[GitHub](https://www.github.com/${responses.github}) 
+### Email
+${responses.email}
+${myMarkdown}`, function(err) {
+    if (err) throw err;
+    console.log('README created!');
+    
+})
+})
+};
 
 // Function call to initialize app
 writeReadMe();
